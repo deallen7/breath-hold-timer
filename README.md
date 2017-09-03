49ab204b2f2cf7c796613e3366ebdc735fde3a71
David Allen <deallen@Davids-MacBook-Pro-2.local>
2017-09-03 17:12:04 -0400

    added timer.rb and updated REAMME

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # breath-hold-timer
+
+This is a README file.
+
+I created a timer. This timer counts to 180 seconds. Yes, I know phones, clocks and timers already exist. No, I don't think this is innovative. Yes, I had fun doing it. 
+
+Cheers :)
---
+++ timer.rb
@@ -0,0 +1,361 @@
+puts "This is a timer for holding your breath and other fun things!"
+sleep(1)
+puts "1"
+sleep(1)
+puts "2"
+sleep(1)
+puts "3"
+sleep(1)
+puts "4"
+sleep(1)
+puts "5"
+sleep(1)
+puts "6"
+sleep(1)
+puts "7"
+sleep(1)
+puts "8"
+sleep(1)
+puts "9"
+sleep(1)
+puts "10"
+sleep(1)
+puts "11"
+sleep(1)
+puts "12"
+sleep(1)
+puts "13"
+sleep(1)
+puts "14"
+sleep(1)
+puts "15"
+sleep(1)
+puts "16"
+sleep(1)
+puts "17"
+sleep(1)
+puts "18"
+sleep(1)
+puts "19"
+sleep(1)
+puts "20"
+sleep(1)
+puts "21"
+sleep(1)
+puts "22"
+sleep(1)
+puts "23"
+sleep(1)
+puts "24"
+sleep(1)
+puts "25"
+sleep(1)
+puts "26"
+sleep(1)
+puts "27"
+sleep(1)
+puts "28"
+sleep(1)
+puts "29"
+sleep(1)
+puts "30"
+sleep(1)
+puts "31"
+sleep(1)
+puts "32"
+sleep(1)
+puts "33"
+sleep(1)
+puts "34"
+sleep(1)
+puts "35"
+sleep(1)
+puts "36"
+sleep(1)
+puts "37"
+sleep(1)
+puts "38"
+sleep(1)
+puts "39"
+sleep(1)
+puts "40"
+sleep(1)
+puts "41"
+sleep(1)
+puts "42"
+sleep(1)
+puts "43"
+sleep(1)
+puts "44"
+sleep(1)
+puts "45"
+sleep(1)
+puts "46"
+sleep(1)
+puts "47"
+sleep(1)
+puts "48"
+sleep(1)
+puts "49"
+sleep(1)
+puts "50"
+sleep(1)
+puts "51"
+sleep(1)
+puts "52"
+sleep(1)
+puts "53"
+sleep(1)
+puts "54"
+sleep(1)
+puts "55"
+sleep(1)
+puts "56"
+sleep(1)
+puts "57"
+sleep(1)
+puts "58"
+sleep(1)
+puts "59"
+sleep(1)
+puts "60"
+sleep(1)
+puts "61"
+sleep(1)
+puts "62"
+sleep(1)
+puts "63"
+sleep(1)
+puts "64"
+sleep(1)
+puts "65"
+sleep(1)
+puts "66"
+sleep(1)
+puts "67"
+sleep(1)
+puts "68"
+sleep(1)
+puts "69"
+sleep(1)
+puts "70"
+sleep(1)
+puts "71"
+sleep(1)
+puts "72"
+sleep(1)
+puts "73"
+sleep(1)
+puts "74"
+sleep(1)
+puts "75"
+sleep(1)
+puts "76"
+sleep(1)
+puts "77"
+sleep(1)
+puts "78"
+sleep(1)
+puts "79"
+sleep(1)
+puts "80"
+sleep(1)
+puts "81"
+sleep(1)
+puts "82"
+sleep(1)
+puts "83"
+sleep(1)
+puts "84"
+sleep(1)
+puts "85"
+sleep(1)
+puts "86"
+sleep(1)
+puts "87"
+sleep(1)
+puts "88"
+sleep(1)
+puts "89"
+sleep(1)
+puts "90"
+sleep(1)
+puts "91"
+sleep(1)
+puts "92"
+sleep(1)
+puts "93"
+sleep(1)
+puts "94"
+sleep(1)
+puts "95"
+sleep(1)
+puts "96"
+sleep(1)
+puts "97"
+sleep(1)
+puts "98"
+sleep(1)
+puts "99"
+sleep(1)
+puts "100"
+sleep(1)
+puts "101"
+sleep(1)
+puts "102"
+sleep(1)
+puts "103"
+sleep(1)
+puts "104"
+sleep(1)
+puts "105"
+sleep(1)
+puts "106"
+sleep(1)
+puts "107"
+sleep(1)
+puts "108"
+sleep(1)
+puts "109"
+sleep(1)
+puts "110"
+sleep(1)
+puts "111"
+sleep(1)
+puts "112"
+sleep(1)
+puts "113"
+sleep(1)
+puts "114"
+sleep(1)
+puts "115"
+sleep(1)
+puts "116"
+sleep(1)
+puts "117"
+sleep(1)
+puts "118"
+sleep(1)
+puts "119"
+sleep(1)
+puts "120"
+sleep(1)
+puts "121"
+sleep(1)
+puts "122"
+sleep(1)
+puts "123"
+sleep(1)
+puts "124"
+sleep(1)
+puts "125"
+sleep(1)
+puts "126"
+sleep(1)
+puts "127"
+sleep(1)
+puts "128"
+sleep(1)
+puts "129"
+sleep(1)
+puts "130"
+sleep(1)
+puts "131"
+sleep(1)
+puts "132"
+sleep(1)
+puts "133"
+sleep(1)
+puts "134"
+sleep(1)
+puts "135"
+sleep(1)
+puts "136"
+sleep(1)
+puts "137"
+sleep(1)
+puts "138"
+sleep(1)
+puts "139"
+sleep(1)
+puts "140"
+sleep(1)
+puts "141"
+sleep(1)
+puts "142"
+sleep(1)
+puts "143"
+sleep(1)
+puts "144"
+sleep(1)
+puts "145"
+sleep(1)
+puts "146"
+sleep(1)
+puts "147"
+sleep(1)
+puts "148"
+sleep(1)
+puts "149"
+sleep(1)
+puts "150"
+sleep(1)
+puts "151"
+sleep(1)
+puts "152"
+sleep(1)
+puts "153"
+sleep(1)
+puts "154"
+sleep(1)
+puts "155"
+sleep(1)
+puts "156"
+sleep(1)
+puts "157"
+sleep(1)
+puts "158"
+sleep(1)
+puts "159"
+sleep(1)
+puts "160"
+sleep(1)
+puts "161"
+sleep(1)
+puts "162"
+sleep(1)
+puts "163"
+sleep(1)
+puts "164"
+sleep(1)
+puts "165"
+sleep(1)
+puts "166"
+sleep(1)
+puts "167"
+sleep(1)
+puts "168"
+sleep(1)
+puts "169"
+sleep(1)
+puts "170"
+sleep(1)
+puts "171"
+sleep(1)
+puts "172"
+sleep(1)
+puts "173"
+sleep(1)
+puts "174"
+sleep(1)
+puts "175"
+sleep(1)
+puts "176"
+sleep(1)
+puts "177"
+sleep(1)
+puts "178"
+sleep(1)
+puts "179"
+sleep(1)
+puts "180"
